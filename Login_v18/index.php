2f50a4c9ffd8de0ebbd6cbb26428dfcca466f0cd
Gabriel Tavares <101677363+bieltavares@users.noreply.github.com>
2022-09-26 22:09:27 -0300

    Index e Login Alterado

--- Login_v18/index.php
+++ Login_v18/index.php
@@ -46,41 +46,41 @@ if(isset($_SESSION['email'])){
 			<div class="wrap-login100">
 				<form action="../backend/login_valida.php"  method="post" class="login100-form validate-form">
 					<span class="login100-form-title p-b-43">
-						Logo
+						<img src="images/logo_tipo.jpg" alt="" width="250px">
 					</span>
 					
 					
 					<div class="wrap-input100 validate-input" data-validate = "Valid email is required: ex@abc.xyz">
 						<input class="input100" type="email" id="email" name="email">
 						<span class="focus-input100"></span>
-						<span class="label-input100">Email</span>
+						<span class="label-input100">E-mail</span>
 					</div>
 					
 					
 					<div class="wrap-input100 validate-input" data-validate="Password is required">
 						<input class="input100" type="password" name="senha">
 						<span class="focus-input100"></span>
-						<span class="label-input100">Password</span>
+						<span class="label-input100">Senha</span>
 					</div>
 
 					<div class="flex-sb-m w-full p-t-3 p-b-32">
 						<div class="contact100-form-checkbox">
 							<input class="input-checkbox100" id="ckb1" type="checkbox" name="remember-me">
 							<label class="label-checkbox100" for="ckb1">
-								Remember me
+								Lembrar Senha
 							</label>
 						</div>
 
 						<div>
 							<a href="#" class="txt1">
-								Forgot Password?
+								Esqueceu a Senha?
 							</a>
 						</div>
 					</div>
 			
 
 					<div class="container-login100-form-btn">
-						<button type="submit" class="login100-form-btn">
+						<button type="submit" class="login100-form-btn ">
 							Login
 						</button>
 					</div>
