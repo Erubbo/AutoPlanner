2147cf00f9851f53354afd9f9233cc9cddbe9907
TheDoctor205 <101677207+MateusSantos205@users.noreply.github.com>
2023-01-24 22:15:04 -0300

    Avatar Adc

--- backend/includes/navbar.php
+++ backend/includes/navbar.php
@@ -10,7 +10,7 @@ include 'controle_session.php';
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse position-absolute top-50 start-50 translate-middle mt-1 " id="navbarSupportedContent">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <a class="nav-link active fs-6 fw-bold" aria-current="page" href="../../../AutoPlanner/tela_prin.php">Home</a>
@@ -58,27 +58,28 @@ include 'controle_session.php';
           <a class="nav-link active fs-6 fw-bold" aria-current="page" href="../../../AutoPlanner/backend/front/gerar_aulas.php">Gerar Aulas</a>
         </li>
 
+        <li class="nav-item">
+          <a class="nav-link active fs-6 fw-bold" aria-current="page" href="../../../AutoPlanner/backend/front/cadastro_feriados.php">Feriados</a>
+        </li>
+
       </ul>
-
-          <li class="nav-item">
-            <a class="nav-link active fs-6" aria-current="page" href="../../../AutoPlanner/backend/front/gerar_aulas.php">Gerar Aulas</a>
-          </li>
-
-          
-          <li class="nav-item">
-            <a class="nav-link active fs-6" aria-current="page" href="../../../AutoPlanner/backend/front/cadastro_feriados.php">Feriados</a>
-          </li>
-          
-        </ul>
-      </div>
     </div>
-    <form class="d-flex" role="buscar">
-      <input class="input-group input-group-sm mb-3 form-control mt-3" type="search" placeholder="Buscar" aria-label="buscar">
-      <a href="backend/"> <button class="btn btn-outline-dark mt-3 me-2" type="submit"><i class="bi bi-search"></i></button></a>
-      
-    </form>
   </div>
+
+  <form class="d-flex" role="buscar">
+    <input class="input-group input-group-sm mb-3 form-control mt-3" type="search" placeholder="Buscar" aria-label="buscar">
+    <a href="backend/"> <button class="btn btn-outline-dark mt-3 me-2" type="submit"><i class="bi bi-search"></i></button></a>
+  </form>
+
+  <div class="dropdown">
+    <a class="btn dropdown-toggle m-2" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ9r3ogaSmpwNYSaEKRifVaHjwmYsKSW7fC6Q&usqp=CAU" class="rounded-circle" height="26" alt="" loading="lazy" />
+    </a>
+
+    <ul class="dropdown-menu dropdown-menu-end">
+      <li><a class="dropdown-item text-center p-0" href="backend/logout.php">Sair</a></li>
+    </ul>
+  </div>
+
+
 </nav>
-
-
-
